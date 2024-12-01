b0a31303f8e31b65eb8c4699f3d77aa37cce239b
TheFred94 <99641714+TheFred94@users.noreply.github.com>
2024-12-01 12:20:27 +0100

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Frederik Rømer Mølholm on 01/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiLoverTests/EmojiLoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverTests.swift
+//  EmojiLoverTests
+//
+//  Created by Frederik Rømer Mølholm on 01/12/2024.
+//
+
+import Testing
+@testable import EmojiLover
+
+struct EmojiLoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
